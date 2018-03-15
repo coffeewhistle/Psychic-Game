22537e97d3a00085f35e481f1b9c852077608fe7
Cole Hansen <cole.hansen@gmail.com>
2018-03-14 21:05:53 -0700

    Finish game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-window.onload(function() {
+window.onload = function() {
 
 var wins = 0;
 var losses = 0;
@@ -6,19 +6,44 @@ var guessesLeft = 20;
 var guesses = [];
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var gameHTML = 
-    "<p>Guess what letter I'm thinking of!</p>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guesses Left: " + guessesLeft + "</p>" +
-    "<p>Your Guesses so far: " + guesses + "</p>";
-
 function game() {
-    document.querySelector("#game").innerHTML = gameHTML;
     letterToGuess = letters[Math.floor(Math.random()*letters.length)];
+    
+    document.getElementById("guessesLeft").innerHTML = 20;
+    document.getElementById("guesses").innerHTML = "";
+
+    console.log(letterToGuess);
+
+    document.onkeypress = function (event) {
+        var guess = event.key;
+        if (guess.toLowerCase() != guess.toUpperCase() && guess != "Enter" && guess != "Space") {
+            if (guess == letterToGuess) {
+                alert("You win!")
+                ++wins;
+                document.getElementById("wins").innerHTML = wins;
+                reset();
+                game();
+            } else if (guess !== letterToGuess) {
+                guesses.push(guess);
+                --guessesLeft;
+                console.log(guessesLeft);
+                document.getElementById("guesses").innerHTML = guesses;
+                document.getElementById("guessesLeft").innerHTML = guessesLeft;
+            }
+        };
+        if (guessesLeft < 1) {
+            alert("YOU LOSE!");
+            ++losses;
+            reset();
+            game();
+        };
+    };
 };
+game();
 
-document.querySelector("#game").innerHTML = gameHTML;
+function reset() {
+    guesses = [];
+    guessesLeft = 20;
+}
 
-
-});
+};
